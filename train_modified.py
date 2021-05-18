b9b366fcc0267f80747dd52b28bb2cb4d8885144
Srinivasan <srini2dl@gmail.com>
2021-05-18 14:34:08 +0300

    some clean for demo

--- train_modified.py
+++ train_modified.py
@@ -8,10 +8,10 @@ from train import train
 
 def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--displayiters", default=10, type=int,
+    parser.add_argument("--displayiters", default=1, type=int,
                     help="Logs will be displayed after every displayiters steps")
-    parser.add_argument("--saveiters", default=500, type=int, help="model will be saved after every saveiters steps")
-    parser.add_argument("--maxiters", default=2000, type=int, help="Max iterations to run")
+    parser.add_argument("--saveiters", default=10, type=int, help="model will be saved after every saveiters steps")
+    parser.add_argument("--maxiters", default=20, type=int, help="Max iterations to run")
     parser.add_argument("--max_snapshots_to_keep", default=4, type=int, help="How many models to save")
     parser.add_argument("--keepdeconvweights", default=True, type=bool)
     args = parser.parse_args()
